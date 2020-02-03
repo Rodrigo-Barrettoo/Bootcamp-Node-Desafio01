3c596c044b20364ad9b836e139d001924963af78
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-02-03 16:45:42 -0300

    Criação das rotas  post para projects e tasks e listagem

--- src/index.js
+++ src/index.js
@@ -4,8 +4,36 @@ const server = express();
 
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  return res.json({teste: 'ok'});
+const projects = []
+
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  }
+
+  projects.push(project)
+
+  return res.status(200).json(projects)
+});
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const { id } = req.params
+  const { task } = req.body;
+
+
+  const project = projects.find(project => project.id == id);
+
+  project.tasks.push(task);
+
+  return res.json(project);
 });
 
 
